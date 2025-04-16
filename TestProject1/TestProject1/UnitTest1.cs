cee97bf848e014d3e24c9de007bf9e1b2433088f
tslape <tslape@users.noreply.github.com>
2025-04-16 15:23:59 -0400

    Commit

---
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+
+namespace TestProject1;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Thread.Sleep(8000);
+    }
+}
+
+public class UnitTest2
+{
+    [Fact]
+    public void Test2()
+    {
+        Thread.Sleep(5000);
+    }
+}
+
+public class UnitTest3
+{
+    [Fact]
+    public void Test3()
+    {
+        Thread.Sleep(5000);
+    }
+}
+public class UnitTest4
+{
+    [Fact]
+    public void Test4()
+    {
+        Thread.Sleep(5000);
+    }
+}
---
+++ TestProject1/TestProject2/Class1.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace TestProject2;
+
+public class UnitTest5
+{
+    [Fact]
+    public void Test5()
+    {
+        Thread.Sleep(5000);
+    }
+}
+
+public class UnitTest6
+{
+    [Fact]
+    public void Test6()
+    {
+        Thread.Sleep(5000);
+    }
+}
+
+public class UnitTest7
+{
+    [Fact]
+    public void Test7()
+    {
+        Thread.Sleep(5000);
+    }
+}
+
+public class UnitTest8
+{
+    [Fact]
+    public void Test8()
+    {
+        Thread.Sleep(5000);
+    }
+}
+
+public class UnitTest9
+{
+    [Fact]
+    public void Test9()
+    {
+        Thread.Sleep(5000);
+    }
+}
